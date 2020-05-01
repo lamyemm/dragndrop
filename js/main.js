535e654deebe788b52eb0ebb9d2ed616f57338e6
lamyemm <lamyemm@gmail.com>
2020-05-01 19:25:45 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+// Fill listeners
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+// Loop through empties and call drag events
+for (let empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
+// Drag functions
+function dragStart() {
+  this.className += " hold";
+  requestAnimationFrame(() => (this.className = "invisible"))
+}
+
+function dragEnd() {
+  this.className = "fill";
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += " hovered";
+}
+
+function dragLeave() {
+  this.className = "empty";
+}
+
+function dragDrop() {
+    this.className = "empty"
+    this.append(fill)
+}
